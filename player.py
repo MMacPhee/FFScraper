ce6c96d007c2677039b6350dc31ecaad1a39767e
MMacPhee <malcolm.n.macphee@gmail.com>
2017-05-04 14:03:06 -0700

    Started stats scrapping method
    laid the groundwork for the method but it doesn’t quite work properly.
    it doesn’t actually collect stats.

--- player.py
+++ player.py
@@ -18,21 +18,4 @@ class Player:
         return url
 
     def get_stats(self):
-        url = self.create_pfr_url()
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, "html.parser")
-        results = soup.find("table", {"id": "stats"})
-        table = results.find("tbody")
-        entries = table.findAll("tr")
-
-        i = 0
-        for rows in entries:
-            self.stats[i]["week"] = rows.find("td", {"data-stat": "opp"})
-            self.stats[i]["opponent"] = rows.find("td", {"data-stat": "opp"})
-            self.stats[i]["rushingyards"] = rows.find("td", {"id": "rush_yds"})
-            self.stats[i]["receivingyards"] = rows.find("td", {"id": "rec_yds"})
-            i += 1
-            print(self.stats[i]["week"], "a",
-                  self.stats[i]["opponent"], "b",
-                  self.stats[i]["rushingyards"], "c",
-                  self.stats[i]["receivingyards"], "d")
+        return self
